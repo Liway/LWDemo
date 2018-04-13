3d3a54727195c841421b3d871c48cee636c03b0d
Liway <sunliwei@bhaigou.com>
2018-04-13 11:31:58 +0800

    uu

---
+++ LWDemo/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  LWDemo
+//
+//  Created by Liway on 2018/4/13.
+//  Copyright © 2018年 PinPinHang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let Car: String = ""
+        print(Car)
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+
+
+}
+
