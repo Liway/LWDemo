6673105e8b753f5bce9fcd58eb3a51a08cd4a4de
Liway <sunliwei@bhaigou.com>
2018-04-13 13:47:41 +0800

    add a label

--- LWDemo/ViewController.swift
+++ LWDemo/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var text_label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -19,6 +20,8 @@ class ViewController: UIViewController {
         Car = "Audi"
         
         print(reverse(text: "stressed"))
+        
+        self.text_label.text = reverse(text: "stressed")
     }
     
     func reverse(text: String) -> String {
